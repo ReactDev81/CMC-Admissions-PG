b2d603ca458aad8cd229a76915ed80139018be43
ReactDev81 <reactdev81@gmail.com>
2025-01-22 18:27:31 +0530

    working on status and add user or edit user

--- src/pages/dashboard/admin/users/popup/AddNewUser.jsx
+++ src/pages/dashboard/admin/users/popup/AddNewUser.jsx
@@ -177,7 +177,7 @@ const AddNewUser = ({onClose}) => {
                 <div>
                     <Checkbox
                         label="Force user to reset password"
-                        id="password_reset"
+                        id="force_reset_password"
                         register={register}
                     />
                     <p className="ml-6 text-black-200">( if this field is checked, then user will be forced to reset password after login )</p>
--- src/pages/dashboard/admin/users/popup/EditUser.jsx
+++ src/pages/dashboard/admin/users/popup/EditUser.jsx
@@ -146,7 +146,7 @@ const EditUser = ({ data, onClose }) => {
             <div>
                 <Checkbox
                     label="Force user to reset password"
-                    id="password_reset"
+                    id="force_reset_password"
                     register={register}
                 />
                 <p className="ml-6 text-black-200">( if this field is checked, then user will be forced to reset password after login )</p>
